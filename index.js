a4a17cb9e434a5883872da01c855f098eb60679d
Shreya Ravindra <1ds23cb052@dsce.edu.in>
2025-11-19 17:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require(&#39;./app&#39;);
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =&gt; {
-console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
